7adca5f4267ff38791c649b954213d8657cc4f2c
Lucas Ducourneau <lucas.ducourneau@etu.u-bordeaux.fr>
2022-07-05 15:57:09 +0200

    Update ordre data

--- arduino/prog_all_sensors/prog_all_sensors.ino
+++ arduino/prog_all_sensors/prog_all_sensors.ino
@@ -3,27 +3,19 @@
 
 int rainPin = A0;
 int lightPin = A5;
-int greenLED = 6;
-int redLED = 7;
 
 // you can adjust the threshold value
 int thresholdValue = 600;
 const int light_seuil = 500;
-
 
 DHT dht(2, DHT11);
 
 void setup() {
    dht.begin();
    pinMode(rainPin, INPUT);
-   pinMode(greenLED, OUTPUT);
-   pinMode(redLED, OUTPUT);
    pinMode(lightPin, INPUT);
-   digitalWrite(greenLED, LOW);
-   digitalWrite(redLED, LOW);
    Serial.begin(9600);
 }
-
 
 
 void loop() {
@@ -32,54 +24,45 @@ void loop() {
 
   int sensorValue = analogRead(rainPin);
   int lightValue = analogRead(lightPin);
-
-  Serial.print("Light value : ");
-  Serial.println(lightValue);
    
-  if (h>50){
-   Serial.print("Humidity: ");
+  if (h>50){  
+   // température
+   Serial.print(t);
+   Serial.print(" - ");
+   Serial.print(lightValue);
+   Serial.print(" - ");
+
+   // humidité
    Serial.print(h);
-   Serial.println(" : temps humide ");
-   Serial.print("Temperature: ");
-   Serial.println(t);
+   Serial.print(" - ");
+
+    if(sensorValue <= thresholdValue){
+      // pluie détectée
+      Serial.print(sensorValue);
+      Serial.print(" - ");
+    } else {
+      // pas de pluie
+      Serial.print(sensorValue);
+      Serial.print(" - ");
+    }
+  } else {
+    Serial.print(t);
+    Serial.print(" - ");
+    Serial.print(lightValue);
+    Serial.print(" - ");
+    Serial.print(h);
+    Serial.print(" - ");
 
     if(sensorValue <= thresholdValue){
       Serial.print(sensorValue);
-      Serial.println(" - Il pleut");
-      digitalWrite(greenLED, LOW);
-      digitalWrite(redLED, HIGH);
-  }
-    else {
-      Serial.print(sensorValue);
-     Serial.println(" - c'est sec");
-     digitalWrite(greenLED, HIGH);
-     digitalWrite(redLED, LOW);
-    }
-  }
-  else{
-   Serial.print("Humidity: ");
-   Serial.print(h);
-   Serial.println(" : temps sec ");
-   Serial.print("Temperature: ");
-   Serial.println(t);
-    if(sensorValue <= thresholdValue){
-      Serial.print(sensorValue);
-      Serial.println(" - Il pleut");
-      digitalWrite(greenLED, LOW);
-      digitalWrite(redLED, HIGH);
      }
     else {
       Serial.print(sensorValue);
-      Serial.println(" - c'est sec");
-      digitalWrite(greenLED, HIGH);
-      digitalWrite(redLED, LOW);
     }
-     
-    }
+  }
  
   // read the input on analog pin 0:
     Serial.println("");
-    Serial.println("");  
 
-  delay(1000);
+  delay(6000);
 }
